223e9f6d25fc64d0da624d0f8a724d50e459627f
Chris Churas <churas.camera@gmail.com>
2022-12-22 10:27:19 -0600

    added null check on saveAsText and some docs

--- src/main/java/org/cytoscape/cyndex2/internal/ui/StringMatchRowFilter.java
+++ src/main/java/org/cytoscape/cyndex2/internal/ui/StringMatchRowFilter.java
@@ -8,11 +8,16 @@ import javax.swing.RowFilter;
 import org.cytoscape.cyndex2.internal.ui.swing.MyNetworksTableModel;
 
 /**
- *
+ * JTable model row filter
  * @author churas
  */
 public class StringMatchRowFilter {
 	
+	/**
+	 * Keeps rows that contains {@code saveAsText} in name field
+	 * @param saveAsText text that should be within name field of row
+	 * @return true if there is a match, false otherwise
+	 */
 	public static RowFilter<MyNetworksTableModel, Object> getStringMatchRowFilter(final String saveAsText){
 		return new RowFilter<MyNetworksTableModel, Object>(){
 			@Override
@@ -23,7 +28,9 @@ public class StringMatchRowFilter {
 				if (networkName == null && (saveAsText == null || saveAsText.trim().isEmpty())){
 					return true;
 				}
-				if (networkName != null && networkName.contains(saveAsText)){
+				// the saveAsText null check is really needed
+				if (networkName != null && saveAsText != null &&
+						networkName.contains(saveAsText)){
 					return true;
 				}
 				return false;
