e9f98fb2522068655937687c211d9d5c1bddc641
Chris Churas <churas.camera@gmail.com>
2023-01-30 16:33:48 -0800

    added more checks to handle case where url is null which is possible

--- src/main/java/org/cytoscape/cyndex2/internal/util/ServerKey.java
+++ src/main/java/org/cytoscape/cyndex2/internal/util/ServerKey.java
@@ -17,19 +17,56 @@ public class ServerKey {
 			this.url = server.getUrl();
 		}
 		
+		/**
+		 * Hashcode of username and url concatenated by @ symbol
+		 * unless one is null in which case the hashcode of the other
+		 * is used. If both are null, 0 is returned
+		 * 
+		 * @return Hashcode or 0 if both username and url are null
+		 */
 		public int hashCode() {
-			return username != null ? username.concat("@").concat(url).hashCode() : url.hashCode();
+			if (username == null && url == null){
+				return 0;
+			}
+			if (username != null && url == null){
+				return username.hashCode();
+			}
+			if (username == null){
+				return url.hashCode();
+			}
+			return username.concat("@").concat(url).hashCode();
 		}
 		
+		/**
+		 * Considered equal if of same type and username and url match where
+		 * if both are null is considered a match. When comparing the username and
+		 * url the .equals() is used for string comparison.
+		 * 
+		 * @param object what to compare
+		 * @return true if both usernames are equal or null and if both urls are null
+		 *         or equal. otherwise false
+		 */
 		public boolean equals(Object object) {
 			if (!(object instanceof ServerKey)) { return false; }
 			
 			ServerKey serverKey = (ServerKey) object;
 			if (serverKey.username == null && this.username == null) {
+				if (serverKey.url == null && this.url == null){
+					return true;
+				}
+				if (serverKey.url == null || this.url == null){
+					return false;
+				}
 				return serverKey.url.equals(this.url);
 			} else if (serverKey.username == null || this.username == null) {
 				return false;
+			} else if (serverKey.url == null && this.url == null){
+				if (serverKey.username == null || this.username == null){
+					return false;
+				}
+				return serverKey.username.equals(this.username);
 			} else {
+				
 				return this.username.equals(serverKey.username) && this.url.equals(serverKey.url);
 			}
 		}
