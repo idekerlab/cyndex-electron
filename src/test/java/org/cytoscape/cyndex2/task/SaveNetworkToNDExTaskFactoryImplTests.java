0a280aed8ca984d5be75875bbe3361d6630cb0e9
Chris Churas <churas.camera@gmail.com>
2023-02-06 16:48:02 -0800

    added more tests

--- src/test/java/org/cytoscape/cyndex2/task/SaveNetworkToNDExTaskFactoryImplTests.java
+++ src/test/java/org/cytoscape/cyndex2/task/SaveNetworkToNDExTaskFactoryImplTests.java
@@ -1,8 +1,22 @@
 package org.cytoscape.cyndex2.task;
 
+import java.util.UUID;
+import org.cytoscape.application.CyApplicationManager;
+import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.cyndex2.internal.task.CanceledTask;
 import org.cytoscape.cyndex2.internal.task.SaveNetworkToNDExTaskFactoryImpl;
+import org.cytoscape.cyndex2.internal.ui.swing.ShowDialogUtil;
+import org.cytoscape.cyndex2.internal.util.NDExNetworkManager;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyRow;
+import org.cytoscape.model.CyTable;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.work.TaskIterator;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -10,143 +24,75 @@ import org.junit.Test;
  */
 public class SaveNetworkToNDExTaskFactoryImplTests {
 	
+	
+	
 	@Test
 	public void testIsReady(){
 		SaveNetworkToNDExTaskFactoryImpl fac = new SaveNetworkToNDExTaskFactoryImpl(null, false, 0l);
 		assertTrue(fac.isReady());
 	}
 	
-	/**
 	@Test
-	public void testCreateTaskIteratorCreateGUIReturnedFalse(){
+	public void testCreateTaskIteratorCurrentNetworkNull(){
 		CyServiceRegistrar mockRegistrar = mock(CyServiceRegistrar.class);
+		
+		CyApplicationManager mockAppManager = mock(CyApplicationManager.class);
+		when(mockAppManager.getCurrentNetwork()).thenReturn(null);
+		when(mockRegistrar.getService(CyApplicationManager.class)).thenReturn(mockAppManager);
+		
 		CySwingApplication mockSwing = mock(CySwingApplication.class);
+		when(mockSwing.getJFrame()).thenReturn(null);
 		when(mockRegistrar.getService(CySwingApplication.class)).thenReturn(mockSwing);
 		
-		OpenNetworkDialog mockOpenDialog = mock(OpenNetworkDialog.class);
-		when(mockOpenDialog.createGUI()).thenReturn(Boolean.FALSE);
-		OpenNetworkFromNDExTaskFactoryImpl fac = new OpenNetworkFromNDExTaskFactoryImpl(mockRegistrar,
-				mockOpenDialog, new ShowDialogUtil());
-		
+		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
+		SaveNetworkToNDExTaskFactoryImpl fac = new SaveNetworkToNDExTaskFactoryImpl(mockRegistrar, false, 0l, mockDialogUtil, null);
 		TaskIterator ti = fac.createTaskIterator();
 		assertNotNull(ti);
 		assertEquals(1, ti.getNumTasks());
 		assertTrue(ti.next() instanceof CanceledTask);
 		verify(mockRegistrar).getService(CySwingApplication.class);
-		verify(mockOpenDialog).createGUI();
+		verify(mockDialogUtil).showMessageDialog(null, "Please select a network to save\n");
+		
 	}
 	
+	/**
 	@Test
-	public void testCreateTaskIteratorSuccess() throws IOException, NdexException {
+	public void testCreateTaskIteratorNoNDExCredentials(){
 		CyServiceRegistrar mockRegistrar = mock(CyServiceRegistrar.class);
+		
+		CyApplicationManager mockAppManager = mock(CyApplicationManager.class);
+		CyNetwork mockNetwork = mock(CyNetwork.class);
+		CyRow mockRow = mock(CyRow.class);
+		when(mockNetwork.getRow(mockNetwork)).thenReturn(mockRow);
+		Long networkSUID = 1l;
+		when(mockNetwork.getSUID()).thenReturn(1l);
+		CyTable mockTable = mock(CyTable.class);
+		CyRow mockSUIDROW = mock(CyRow.class);
+		UUID ndexUUID = UUID.randomUUID();
+		when(mockSUIDROW.get(NDExNetworkManager.UUID_COLUMN, String.class)).thenReturn(ndexUUID.toString());
+		when(mockTable.getRow(networkSUID)).thenReturn(mockSUIDROW);
+		when(mockNetwork.getTable(CyNetwork.class, CyNetwork.HIDDEN_ATTRS)).thenReturn(mockTable);
+		when(mockRow.get(CyNetwork.NAME, String.class)).thenReturn(null);
+		
+		
+		when(mockAppManager.getCurrentNetwork()).thenReturn(mockNetwork);
+		
+		when(mockRegistrar.getService(CyApplicationManager.class)).thenReturn(mockAppManager);
+		
 		CySwingApplication mockSwing = mock(CySwingApplication.class);
 		when(mockSwing.getJFrame()).thenReturn(null);
 		when(mockRegistrar.getService(CySwingApplication.class)).thenReturn(mockSwing);
 		
-		Object[] options = {new JButton("Open"), new JButton("cancel")};
-		
-		OpenNetworkDialog mockOpenDialog = mock(OpenNetworkDialog.class);
-		when(mockOpenDialog.createGUI()).thenReturn(Boolean.TRUE);
-		when(mockOpenDialog.getMainOpenButton()).thenReturn((JButton)options[0]);
-		when(mockOpenDialog.getMainCancelButton()).thenReturn((JButton)options[1]);
-		
-		NetworkSummary selectedNetwork = new NetworkSummary();
-		selectedNetwork.setExternalId(UUID.randomUUID());
-		when(mockOpenDialog.getNDExSelectedNetwork()).thenReturn(selectedNetwork);
-		
 		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
-		when(mockDialogUtil.showOptionDialog(null, mockOpenDialog, "Open", JOptionPane.YES_NO_OPTION,
-				JOptionPane.PLAIN_MESSAGE, null, options, options[0])).thenReturn(0);
-		
-		ServerManager mockServerManager = mock(ServerManager.class);
-		Server mockServer = mock(Server.class);
-		when(mockServerManager.getServer()).thenReturn(mockServer);
-		NdexRestClientModelAccessLayer mockMal = mock(NdexRestClientModelAccessLayer.class);
-		when(mockMal.getNetworkSummaryById(selectedNetwork.getExternalId(), null)).thenReturn(selectedNetwork);
-		when(mockServer.getModelAccessLayer()).thenReturn(mockMal);
-		ServerManager.INSTANCE = mockServerManager;
-		
-		
-		OpenNetworkFromNDExTaskFactoryImpl fac = new OpenNetworkFromNDExTaskFactoryImpl(mockRegistrar,
-				mockOpenDialog, mockDialogUtil);
-		
-		TaskIterator ti = fac.createTaskIterator();
-		assertNotNull(ti);
-		assertEquals(1, ti.getNumTasks());
-		assertTrue(ti.next() instanceof NetworkImportTask);
-		
-	}
-	
-	@Test
-	public void testCreateTaskIteratorGetMalThrowsException() throws IOException, NdexException {
-		CyServiceRegistrar mockRegistrar = mock(CyServiceRegistrar.class);
-		CySwingApplication mockSwing = mock(CySwingApplication.class);
-		when(mockSwing.getJFrame()).thenReturn(null);
-		when(mockRegistrar.getService(CySwingApplication.class)).thenReturn(mockSwing);
-		
-		Object[] options = {new JButton("Open"), new JButton("cancel")};
-		
-		OpenNetworkDialog mockOpenDialog = mock(OpenNetworkDialog.class);
-		when(mockOpenDialog.createGUI()).thenReturn(Boolean.TRUE);
-		when(mockOpenDialog.getMainOpenButton()).thenReturn((JButton)options[0]);
-		when(mockOpenDialog.getMainCancelButton()).thenReturn((JButton)options[1]);
-		
-		NetworkSummary selectedNetwork = new NetworkSummary();
-		selectedNetwork.setExternalId(UUID.randomUUID());
-		when(mockOpenDialog.getNDExSelectedNetwork()).thenReturn(selectedNetwork);
-		
-		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
-		when(mockDialogUtil.showOptionDialog(null, mockOpenDialog, "Open", JOptionPane.YES_NO_OPTION,
-				JOptionPane.PLAIN_MESSAGE, null, options, options[0])).thenReturn(0);
-		
-		ServerManager mockServerManager = mock(ServerManager.class);
-		Server mockServer = mock(Server.class);
-		when(mockServerManager.getServer()).thenReturn(mockServer);
-		NdexRestClientModelAccessLayer mockMal = mock(NdexRestClientModelAccessLayer.class);
-		when(mockMal.getNetworkSummaryById(selectedNetwork.getExternalId(), null)).thenThrow(new NdexException("error"));
-		when(mockServer.getModelAccessLayer()).thenReturn(mockMal);
-		ServerManager.INSTANCE = mockServerManager;
-		
-		
-		OpenNetworkFromNDExTaskFactoryImpl fac = new OpenNetworkFromNDExTaskFactoryImpl(mockRegistrar,
-				mockOpenDialog, mockDialogUtil);
-		
+		SaveNetworkToNDExTaskFactoryImpl fac = new SaveNetworkToNDExTaskFactoryImpl(mockRegistrar, false, 0l, mockDialogUtil, null);
 		TaskIterator ti = fac.createTaskIterator();
 		assertNotNull(ti);
 		assertEquals(1, ti.getNumTasks());
 		assertTrue(ti.next() instanceof CanceledTask);
-		
-	}
-	
-	@Test
-	public void testCreateTaskIteratorNetworkSummaryIsNull() throws IOException, NdexException {
-		CyServiceRegistrar mockRegistrar = mock(CyServiceRegistrar.class);
-		CySwingApplication mockSwing = mock(CySwingApplication.class);
-		when(mockSwing.getJFrame()).thenReturn(null);
-		when(mockRegistrar.getService(CySwingApplication.class)).thenReturn(mockSwing);
-		
-		Object[] options = {new JButton("Open"), new JButton("cancel")};
-		
-		OpenNetworkDialog mockOpenDialog = mock(OpenNetworkDialog.class);
-		when(mockOpenDialog.createGUI()).thenReturn(Boolean.TRUE);
-		when(mockOpenDialog.getMainOpenButton()).thenReturn((JButton)options[0]);
-		when(mockOpenDialog.getMainCancelButton()).thenReturn((JButton)options[1]);
-		
-
-		when(mockOpenDialog.getNDExSelectedNetwork()).thenReturn(null);
-		
-		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
-		when(mockDialogUtil.showOptionDialog(null, mockOpenDialog, "Open", JOptionPane.YES_NO_OPTION,
-				JOptionPane.PLAIN_MESSAGE, null, options, options[0])).thenReturn(0);
-		
-		OpenNetworkFromNDExTaskFactoryImpl fac = new OpenNetworkFromNDExTaskFactoryImpl(mockRegistrar,
-				mockOpenDialog, mockDialogUtil);
-		
-		TaskIterator ti = fac.createTaskIterator();
-		assertNotNull(ti);
-		assertEquals(1, ti.getNumTasks());
-		assertTrue(ti.next() instanceof CanceledTask);
+		verify(mockRegistrar).getService(CySwingApplication.class);
+		verify(mockDialogUtil).showMessageDialog(null, "Please select a network to save\n");
 		
 	}
 	*/
+	
 }
